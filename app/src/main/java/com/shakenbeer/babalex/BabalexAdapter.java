dc1ae0510f611d9ab0c422c778a3a1c9889b442b
o-nos <oleksandr.nos@softermii.com>
2017-08-21 16:40:00 +0300

    - started working on design implementation.

--- app/src/main/java/com/shakenbeer/babalex/BabalexAdapter.java
+++ app/src/main/java/com/shakenbeer/babalex/BabalexAdapter.java
@@ -4,7 +4,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewStub;
 
 import com.shakenbeer.babalex.data.Babalex;
 
@@ -31,7 +30,7 @@ public class BabalexAdapter extends RecyclerView.Adapter<BabalexAdapter.BabalexH
 
     @Override
     public void onBindViewHolder(BabalexHolder holder, int position) {
-        holder.image.setImageResource(items.get(position).getImage());
+        holder.image.setImageResource(items.get(position).getImageRes());
     }
 
     public Babalex getItem(int adapterPos) {
